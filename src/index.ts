bf02f8fee207ad69c36de6d997f308d039660371
Your New Username <sahupravin960@gmail.com>
2024-09-08 11:38:45 +0530

    chore:reder-spin-down-soln

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import axios from "axios";
+
+const url = `https://synclistener-backend.onrender.com/health`;
+
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+//
